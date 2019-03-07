03215bf2a63255562d0ef88b6b6e16e9811b1107
Patrick Leroux <37802522+PatLeroux@users.noreply.github.com>
2019-03-07 12:14:37 -0500

    Treehouse FSJS Ajax/JSON em

--- js/widget.js
+++ js/widget.js
@@ -19,3 +19,24 @@ xhr.onreadystatechange = () => {
 };
 xhr.open('GET', 'data/employees.json');
 xhr.send();
+
+var xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = function() {
+    if (xhr2.readyState === 4 && xhr2.status === 200) {
+        var rooms = JSON.parse(xhr2.responseText);
+        var statusHTML = '<ul class="rooms">';
+        for (var i = 0; i < rooms.length; i += 1) {
+            if (rooms[i].available === true) {
+                statusHTML += '<li class="empty">';
+            } else {
+                statusHTML += '<li class="full">';
+            }
+            statusHTML += rooms[i].room;
+            statusHTML += '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('roomList').innerHTML = statusHTML;
+    }
+};
+xhr2.open('GET', 'data/rooms.json');
+xhr2.send();
